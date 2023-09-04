5adad52b5bbd8ba153997b0d8a5f6c17bb3dae93
Jude Shreffler <js467820@ohio.edu>
2023-09-04 16:30:07 -0400

    redoing with pixels

--- src/lib.rs
+++ src/lib.rs
@@ -1,129 +1,88 @@
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::{WindowBuilder, Window},
-};
+use error_iter::ErrorIter as _;
+use log::error;
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{WindowBuilder, Window};
+use winit_input_helper::WinitInputHelper;
 
-const RESOLUTION: i32 = 1;
-const SCREEN_WIDTH: i32 = 160 * RESOLUTION;
-const SCREEN_HEIGHT: i32 = 120 * RESOLUTION;
-const PIXEL_SCALE: i32 = 4 / RESOLUTION;
-const GL_SCREEN_WIDTH: i32 = SCREEN_WIDTH * PIXEL_SCALE;
-const GL_SCREEN_HEIGHT: i32 = SCREEN_HEIGHT * PIXEL_SCALE;
+const RESOLUTION: u8 = 1; // 0=160x120, 1=360x240, 4=640x480
+const SCREEN_WIDTH: u32 = (160 * RESOLUTION) as u32;
+const SCREEN_HEIGHT: u32 = (120 * RESOLUTION) as u32;
+const PIXEL_SCALE: u8 = 4 / RESOLUTION;
+pub const BUFFER_WIDTH: u32 = SCREEN_WIDTH * (PIXEL_SCALE as u32);
+pub const BUFFER_HEIGHT: u32 = SCREEN_HEIGHT * (PIXEL_SCALE as u32);
 
-struct State {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>,
-    window: Window
+struct Doom {
+    event_loop: EventLoop<()>,
+    input: WinitInputHelper,
+    render_window: Window,
+    pixel_buffer: Pixels,
+
 }
 
-impl State {
-    // creating some of the wgpu types requires async code
-    async fn new(window: Window) -> Self{
-        let size = window.inner_size();
+impl Doom {
+    fn new() -> Option<Doom> {
+        env_logger::init();
+        let event_loop = EventLoop::new();
+        let mut input = WinitInputHelper::new();
+        let render_window = {
+            let size = LogicalSize::new(BUFFER_WIDTH as f64, BUFFER_HEIGHT as f64);
+            WindowBuilder::new()
+                .with_title("Hello Pixels")
+                .with_inner_size(size)
+                .with_min_inner_size(size)
+                .build(&event_loop)
+                .unwrap()
+        };
 
-        // The instance is a handle to our GPU
-        // Backends::all => Vulkan + Metal + DX12 + BrowserWebGPU
-        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::all(),
-            dx12_shader_compiler: Default::default()
+        let mut pixel_buffer = {
+            let window_size = render_window.inner_size();
+            let surface_texture: SurfaceTexture<'_, winit::window::Window> = SurfaceTexture::new(window_size.width, window_size.height, &render_window);
+            Pixels::new(BUFFER_WIDTH, BUFFER_HEIGHT, surface_texture).unwrap()
+        };
+
+        Some(Doom {event_loop, input, render_window, pixel_buffer})
+    }
+
+    fn run() -> Option<()> {
+        event_loop.run(move |event, _, control_flow| {
+            // Draw the current frame
+            
+    
+            // Handle input events
+            if input.update(&event) {
+                // Close events
+                if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+    
+                // Resize the window
+                if let Some(size) = input.window_resized() {
+                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                        log_error("pixels.resize_surface", err);
+                        *control_flow = ControlFlow::Exit;
+                        return;
+                    }
+                }
+    
+                // Update internal state and request a redraw
+                
+                window.request_redraw();
+            }
         });
 
-        // # Safety
-        //
-        // The surface needs to live as long as the window that created it.
-        // State owns the window so this should be safe
-        let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
-        let adapter = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            },
-        ).await.unwrap();
-
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
-                label: None, 
-            },
-            None,
-        ).await.unwrap();
-
-        let surface_caps = surface.get_capabilities(&adapter);
-        // Shader code in this tutorial assum
+        Some(())
     }
-
-    pub fn window(&self) -> &Window {
-        &self.window
-    }
-
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
-    }
-
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        todo!()
-    }
-
-    fn update(&mut self) {
-        todo!()
-    }
-
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!()
-    }
-}
-
-struct Time {
-    frame_one: i32,
-    frame_two: i32
 }
 
 struct Keys {
-    forward: i32,
-    backward: i32,
-    left: i32,
-    right: i32,
-
-    strafe_left: i32,
-    strafe_right: i32,
-
-    m: i32
+    w: bool, // up, left, down, right
+    a: bool,
+    s: bool,
+    d: bool,
+    sl: bool, // strafe left and right
+    sr: bool, 
 }
-
-fn pixel(x: i32, y: i32, color: i32) {
-
-}
-
-pub fn run() {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        },
-        _ => {}
-    });
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
-use doom_render::run;
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
 
-fn main() {
-    run();
+use error_iter::ErrorIter as _;
+use log::error;
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+fn main() -> Result<(), Error> {
+    
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                    log_error("pixels.resize_surface", err);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+
+            // Update internal state and request a redraw
+            
+            window.request_redraw();
+        }
+    });
+
+    None
+}
+
+fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
+    error!("{method_name}() failed: {err}");
+    for source in err.sources().skip(1) {
+        error!("  Caused by: {source}");
+    }
 }
