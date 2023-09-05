5e35f81bf7bc9b507abd9025c6aede408c2bab55
Jude Shreffler <js467820@ohio.edu>
2023-09-05 10:35:26 -0400

    player turn and forward/backward work and print debug messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-use error_iter::ErrorIter as _;
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::{WindowBuilder, Window};
-use winit_input_helper::WinitInputHelper;
+use std::f32::consts::PI;
+
+use minifb::{Key, Window, WindowOptions};
 
 const RESOLUTION: u8 = 1; // 0=160x120, 1=360x240, 4=640x480
 const SCREEN_WIDTH: u32 = (160 * RESOLUTION) as u32;
@@ -14,75 +9,141 @@ const PIXEL_SCALE: u8 = 4 / RESOLUTION;
 pub const BUFFER_WIDTH: u32 = SCREEN_WIDTH * (PIXEL_SCALE as u32);
 pub const BUFFER_HEIGHT: u32 = SCREEN_HEIGHT * (PIXEL_SCALE as u32);
 
-struct Doom {
-    event_loop: EventLoop<()>,
-    input: WinitInputHelper,
-    render_window: Window,
-    pixel_buffer: Pixels,
-
+pub struct Doom {
+    buffer: Vec<u32>,
+    window: Window,
+    trig: Trig,
+    player_position: Point3,
+    player_yaw: i32,
+    player_tilt: i32,
 }
 
 impl Doom {
-    fn new() -> Option<Doom> {
-        env_logger::init();
-        let event_loop = EventLoop::new();
-        let mut input = WinitInputHelper::new();
-        let render_window = {
-            let size = LogicalSize::new(BUFFER_WIDTH as f64, BUFFER_HEIGHT as f64);
-            WindowBuilder::new()
-                .with_title("Hello Pixels")
-                .with_inner_size(size)
-                .with_min_inner_size(size)
-                .build(&event_loop)
-                .unwrap()
-        };
+    pub fn new() -> Doom {
+        let mut buffer: Vec<u32> = vec![0; (BUFFER_WIDTH * BUFFER_HEIGHT) as usize];
 
-        let mut pixel_buffer = {
-            let window_size = render_window.inner_size();
-            let surface_texture: SurfaceTexture<'_, winit::window::Window> = SurfaceTexture::new(window_size.width, window_size.height, &render_window);
-            Pixels::new(BUFFER_WIDTH, BUFFER_HEIGHT, surface_texture).unwrap()
-        };
-
-        Some(Doom {event_loop, input, render_window, pixel_buffer})
-    }
-
-    fn run() -> Option<()> {
-        event_loop.run(move |event, _, control_flow| {
-            // Draw the current frame
-            
-    
-            // Handle input events
-            if input.update(&event) {
-                // Close events
-                if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-    
-                // Resize the window
-                if let Some(size) = input.window_resized() {
-                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                        log_error("pixels.resize_surface", err);
-                        *control_flow = ControlFlow::Exit;
-                        return;
-                    }
-                }
-    
-                // Update internal state and request a redraw
-                
-                window.request_redraw();
-            }
+        let mut window = Window::new(
+            "Test - ESC to exit",
+            BUFFER_WIDTH as usize,
+            BUFFER_HEIGHT as usize,
+            WindowOptions::default(),
+        )
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
         });
 
-        Some(())
+        // Limit to max ~60 fps update rate
+        window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+        
+        let trig = Trig::init();
+        let player_position = Point3 { x: 70.0, y: -110.0, z: 20.0 };
+        let player_yaw = 0;
+        let player_tilt = 0;
+
+        Doom {buffer, window, trig, player_position, player_yaw, player_tilt}
+    }
+    
+    pub fn run(&mut self) {
+        while self.window.is_open() && !self.window.is_key_down(Key::Escape) {
+            self.draw_frame();
+        }
+    }
+
+    fn draw_frame(&mut self) {
+        // debug printing
+        self.debug();
+        
+        self.clear_background(0x0);
+        self.draw3D();
+        self.check_inputs();
+
+
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        self.window
+            .update_with_buffer(&self.buffer, BUFFER_WIDTH as usize, BUFFER_HEIGHT as usize)
+            .unwrap();
+    }
+
+    fn debug(&self) {
+        println!("Player position: {} {} {}", self.player_position.x, self.player_position.y, self.player_position.z);
+        println!("Player yaw: {}", self.player_yaw)
+    }
+
+    fn pixel(&mut self, x: u32, y: u32, color: u32) {
+        self.buffer[screen_to_buffer(x, y)] = color;
+    }
+
+    fn clear_background(&mut self, color: u32) {
+        for i in self.buffer.iter_mut() {
+            *i = color;
+        }
+    }
+
+    fn draw3D(&mut self) {
+
+    }
+
+    fn check_inputs(&mut self) {
+        for key in self.window.get_keys() {
+            match key {
+                Key::A => {
+                    self.player_yaw -= 4;
+                    if self.player_yaw < 0 {
+                        self.player_yaw += 360;
+                    }
+                },
+                Key::D => {
+                    self.player_yaw += 4;
+                    if self.player_yaw > 359 {
+                        self.player_yaw -= 360;
+                    }
+                },
+                Key::W => {
+                    let delta_x = self.trig.sin[self.player_yaw as usize] * 10.0;
+                    let delta_y = self.trig.cos[self.player_yaw as usize] * 10.0;
+                    self.player_position.x += delta_x;
+                    self.player_position.y += delta_y;
+                },
+                Key::S => {
+                    let delta_x = self.trig.sin[self.player_yaw as usize] * -5.0;
+                    let delta_y = self.trig.cos[self.player_yaw as usize] * -5.0;
+                    self.player_position.x += delta_x;
+                    self.player_position.y += delta_y;
+                },
+
+                _ => (),
+            }
+        }
     }
 }
 
-struct Keys {
-    w: bool, // up, left, down, right
-    a: bool,
-    s: bool,
-    d: bool,
-    sl: bool, // strafe left and right
-    sr: bool, 
+struct Point3 {
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+struct Trig {
+    sin: [f32; 360],
+    cos: [f32; 360],
+}
+
+impl Trig {
+    fn init() -> Trig {
+        let mut sin: [f32; 360] = [0.0; 360];
+        let mut cos: [f32; 360] = [0.0; 360];
+
+        for i in 0..360 {
+            sin[i] = (i as f32 / 180.0*PI).sin();
+            cos[i] = (i as f32 / 180.0*PI).cos();
+        }
+
+        Trig {sin, cos}
+    }
+}
+
+// takes a screen position starting from the top left and converts it to an index for our buffer
+fn screen_to_buffer(x: u32, y: u32) -> usize {
+    (y * BUFFER_WIDTH + x) as usize
 }
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,5 @@
-#![deny(clippy::all)]
-#![forbid(unsafe_code)]
+use doom_render::{Doom};
 
-use error_iter::ErrorIter as _;
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
-use winit_input_helper::WinitInputHelper;
-
-fn main() -> Result<(), Error> {
-    
-
-    event_loop.run(move |event, _, control_flow| {
-        // Draw the current frame
-        
-
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-            }
-
-            // Update internal state and request a redraw
-            
-            window.request_redraw();
-        }
-    });
-
-    None
-}
-
-fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
-    error!("{method_name}() failed: {err}");
-    for source in err.sources().skip(1) {
-        error!("  Caused by: {source}");
-    }
+fn main() {
+    Doom::new().run();
 }
